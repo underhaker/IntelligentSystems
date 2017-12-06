00f9bcdbfd6b6aca0feb3ba79b397b932ed20851
underhaker <yoan.stoyanov11@gmail.com>
2017-12-06 20:34:44 +0200

    NQueens uses random starting position - but it is slower now; added KNN

---
+++ src/KNN.java
@@ -0,0 +1,215 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class KNN {
+
+    private List<Record> trainingRecords;
+    private List<Record> testingRecords;
+    private List<Integer> usedIndexes;
+    private int testingSize;
+    private final static double TRAINING_PERCENTAGE = 0.2;
+    private final static String DATA_SET = "resources/iris.data.txt";
+
+    public void solveKnn(int k) {
+        List<Record> records = readFromFile(DATA_SET);
+        usedIndexes = new ArrayList<>();
+        testingRecords = getTestingRecords(records);
+        trainingRecords = getTrainingRecords(records);
+        for (Record testingRecord : testingRecords) {
+            List<Record> neighbours = findKNearestNeighbours(trainingRecords, testingRecord, k);
+            String className = classify(neighbours);
+            testingRecord.setPredictedClassName(className);
+        }
+        System.out.println(getPrediction() * 100 + "%");
+    }
+
+    private String classify(List<Record> neighbours) {
+        HashMap<String, Double> map = new HashMap<>();
+        double normalizedDistance;
+        for (Record neighbour : neighbours) {
+            if (!map.containsKey(neighbour.getClassName())) {
+                normalizedDistance = 1 / neighbour.getDistance();
+                map.put(neighbour.getClassName(), 1 / normalizedDistance);
+            } else {
+                double distance = map.get(neighbour.getClassName());
+                normalizedDistance = 1 / neighbour.getDistance();
+                distance += normalizedDistance;
+                map.put(neighbour.getClassName(), distance);
+            }
+        }
+
+        double maxValue = 0;
+        Set<String> classNameSet = map.keySet();
+        Iterator<String> iterator = classNameSet.iterator();
+        String predictedClassName = null;
+        while (iterator.hasNext()) {
+            String className = iterator.next();
+            double distance = map.get(className);
+            if (distance > maxValue) {
+                maxValue = distance;
+                predictedClassName = className;
+            }
+        }
+        return predictedClassName;
+    }
+
+    private List<Record> findKNearestNeighbours(List<Record> trainingRecords, Record testingRecord, int k) {
+        List<Record> neighbours = new ArrayList<>();
+        Record trainingRecord;
+        for (int index = 0; index < k; index++) {
+            trainingRecord = trainingRecords.get(index);
+            trainingRecord.setDistance(getDistance(trainingRecord, testingRecord));
+            neighbours.add(trainingRecord);
+        }
+        for (int index = k; index < trainingRecords.size(); index++) {
+            trainingRecord = trainingRecords.get(index);
+            trainingRecord.setDistance(getDistance(trainingRecord, testingRecord));
+            int maxIndex = 0;
+            for (int j = 1; j < k; j++) {
+                if (neighbours.get(j).getDistance() > neighbours.get(maxIndex).getDistance()) {
+                    maxIndex = j;
+                }
+            }
+            if (neighbours.get(maxIndex).getDistance() > trainingRecord.getDistance()) {
+                neighbours.set(maxIndex, trainingRecord);
+            }
+        }
+        return neighbours;
+    }
+
+    private double getDistance(Record trainingRecord, Record testingRecord) {
+        double sum = 0;
+        for (int i = 0; i < trainingRecord.getAttributes().length; i++) {
+            sum += Math.pow(trainingRecord.getAttributes()[i] - testingRecord.getAttributes()[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    private double getPrediction() {
+        int correctPredictions = 0;
+        for (Record testingRecord : testingRecords) {
+            if (testingRecord.getClassName().equals(testingRecord.getPredictedClassName())) {
+                correctPredictions++;
+            }
+            System.out.println("predicted:" + testingRecord.getPredictedClassName() + "   actual:" + testingRecord.getClassName());
+        }
+        return (double) correctPredictions / testingRecords.size();
+    }
+
+    private List<Record> getTestingRecords(List<Record> records) {
+        Random generator = new Random();
+        Double testingSizeDouble = records.size() * TRAINING_PERCENTAGE;
+        this.testingSize = testingSizeDouble.intValue();
+        int counter = 0;
+        int currentIndex;
+        List<Record> testingRecords = new ArrayList<>();
+        while (counter < testingSize) {
+            while (true) {
+                currentIndex = generator.nextInt(records.size());
+                if (!usedIndexes.contains(currentIndex)) {
+                    break;
+                }
+            }
+            testingRecords.add(records.get(currentIndex));
+            usedIndexes.add(currentIndex);
+            counter++;
+        }
+        return testingRecords;
+    }
+
+
+    private List<Record> getTrainingRecords(List<Record> records) {
+        List<Record> trainingRecords = new ArrayList<>();
+        int index = 0;
+        int estimatedTrainingSize = records.size() - testingSize;
+        while (trainingRecords.size() < estimatedTrainingSize) {
+            if (!usedIndexes.contains(index)) {
+                trainingRecords.add(records.get(index));
+                usedIndexes.add(index);
+            }
+            index++;
+        }
+        return trainingRecords;
+    }
+
+
+    private List<Record> readFromFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            String[] input;
+            List<Record> records = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                input = line.split(",");
+                Record record = new Record();
+                double[] attributes = new double[4];
+                for (int i = 0; i < 4; i++) {
+                    attributes[i] = Double.parseDouble(input[i]);
+                }
+                record.setAttributes(attributes);
+                record.setClassName(input[4]);
+                records.add(record);
+            }
+            return records;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    class Record {
+        private double distance;
+        private double[] attributes;
+        private String className;
+        private String predictedClassName;
+
+        double getDistance() {
+            return distance;
+        }
+
+        void setDistance(double distance) {
+            this.distance = distance;
+        }
+
+        double[] getAttributes() {
+            return attributes;
+        }
+
+        void setAttributes(double[] attributes) {
+            this.attributes = attributes;
+        }
+
+        String getClassName() {
+            return className;
+        }
+
+        void setClassName(String className) {
+            this.className = className;
+        }
+
+        String getPredictedClassName() {
+            return predictedClassName;
+        }
+
+        void setPredictedClassName(String predictedClassName) {
+            this.predictedClassName = predictedClassName;
+        }
+    }
+
+    public static void main(String[] args) {
+        KNN knn = new KNN();
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        knn.solveKnn(k);
+    }
+}
--- src/NQueens.java
+++ src/NQueens.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 
 public class NQueens {
 
+    private static int maxIterations;
     private static Random random = new Random();
     private static int iterations = 0;
 
     private static ChessBoard moveQueen(ChessBoard chessBoard, int col) {
 
         int bestRow = chessBoard.getRowWithLeastCollisions(col);
-        int q = chessBoard.queens[col];
-        chessBoard = updateRow(chessBoard, q, bestRow, col);
-        chessBoard = updateFirstDiagonal(chessBoard, chessBoard.queenBoardIndex(col), bestRow * chessBoard.queens.length + col);
-        chessBoard = updateSecondDiagonal(chessBoard, chessBoard.queenBoardIndex(col), bestRow * chessBoard.queens.length + col);
+//        int q = chessBoard.queens[col];
+//        chessBoard = updateRow(chessBoard, q, bestRow, col);
+//        chessBoard = updateFirstDiagonal(chessBoard, chessBoard.queenBoardIndex(col), bestRow * chessBoard.queens.length + col);
+//        chessBoard = updateSecondDiagonal(chessBoard, chessBoard.queenBoardIndex(col), bestRow * chessBoard.queens.length + col);
         chessBoard.queens[col] = bestRow;
         return chessBoard;
     }
@@ -112,52 +113,81 @@ public class NQueens {
     private static int[] createHistogram(ChessBoard chessBoard) {
         int len = chessBoard.queens.length;
         int[] histogram = new int[len * len];
-        for (int i = 0; i < len; i++) {
-            histogram[i] = len;
-        }
 
-        for (int row = 1; row < len; row++) {
+        for (int row = 0; row < len; row++) {
             for (int col = 0; col < len; col++) {
-                if (row > col) {
-                    if (row + col >= len) {
-                        histogram[row * len + col] = 1;
-                    } else {
-                        histogram[row * len + col] = 2;
-                    }
-                } else {
-                    if (row + col < len) {
-                        histogram[row * len + col] = 3;
-                    } else {
-                        histogram[row * len + col] = 2;
-                    }
-                }
+                histogram[row * len + col] = conflicts(row, col, chessBoard.queens);
             }
         }
+//
+//        for (int i = 1; i <= histogram.length; i++) {
+//            if (i % len == 0) {
+//                System.out.println(histogram[i - 1]);
+//            } else {
+//                System.out.print(histogram[i - 1] + " ");
+//            }
+//        }
+//        System.out.println();
         return histogram;
     }
+
+    private static int conflicts(int row, int col, int[] queens) {
+        int count = 0;
+        for (int c = 0; c < queens.length; c++) {
+            if (c == col) continue;
+            int r = queens[c];
+            if (r == row || Math.abs(r - row) == Math.abs(c - col)) count++;
+        }
+        return count;
+    }
+
 
     private static ChessBoard fillChessBoard(int n) {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.queens = new int[n];
+        Random generator = new Random();
+        int rowIndex;
+        int[] usedRows = new int[n];
         for (int i = 0; i < n; i++) {
-            chessBoard.queens[i] = 0;
+            while (true) {
+                rowIndex = generator.nextInt(n);
+                if (usedRows[rowIndex] == 0) {
+                    usedRows[rowIndex] = 1;
+                    chessBoard.queens[i] = rowIndex;
+                    break;
+                }
+            }
+//            System.out.print(chessBoard.queens[i] + " ");
+//            chessBoard.queens[i] = 0;
         }
+//        System.out.println();
         return chessBoard;
     }
 
-    private static void minConflicts(ChessBoard chessBoard) {
-        while (!chessBoard.solved()) {
+    private static boolean minConflicts(ChessBoard chessBoard) {
+        boolean foundSolution = false;
+        while (iterations < maxIterations) {
             iterations++;
             int col = chessBoard.getColumnWithMostCollisions();
             chessBoard = moveQueen(chessBoard, col);
+            if (chessBoard.solved()) {
+                foundSolution = true;
+                break;
+            }
         }
-
+        System.out.println(iterations);
+        return foundSolution;
     }
 
     public void solveNQueens(int n) {
-        ChessBoard chessBoard = fillChessBoard(n);
-        chessBoard.conflictsOnPosition = createHistogram(chessBoard);
-        minConflicts(chessBoard);
+        maxIterations = 2 * n;
+        boolean foundSolution = false;
+        while (!foundSolution) {
+            iterations = 0;
+            ChessBoard chessBoard = fillChessBoard(n);
+//            chessBoard.conflictsOnPosition = createHistogram(chessBoard);
+            foundSolution = minConflicts(chessBoard);
+        }
     }
 
     public static void main(String[] args) {
@@ -176,16 +206,15 @@ public class NQueens {
         public int getColumnWithMostCollisions() {
             List<Integer> big = new ArrayList<>();
             big.add(0);
-            int currentQueenBoardIndex = queenBoardIndex(0);
-            int largest = conflictsOnPosition[currentQueenBoardIndex];
-
-            for (int col = 1; col < queens.length; col++) {
-                currentQueenBoardIndex = queenBoardIndex(col);
-                if (largest < conflictsOnPosition[currentQueenBoardIndex]) {
+            int largest;
+            int maxConflicts = 0;
+            for (int col = 0; col < queens.length; col++) {
+                largest = conflicts(queens[col], col, queens);
+                if (largest > maxConflicts) {
                     big.clear();
                     big.add(col);
-                    largest = conflictsOnPosition[currentQueenBoardIndex];
-                } else if (largest == conflictsOnPosition[currentQueenBoardIndex]) {
+                    maxConflicts = largest;
+                } else if (largest == maxConflicts) {
                     big.add(col);
                 }
             }
@@ -194,19 +223,21 @@ public class NQueens {
 
         public int getRowWithLeastCollisions(int col) {
             List<Integer> small = new ArrayList<>();
+            int minConflicts = queens.length;
             small.add(col);
-            int leastVal = conflictsOnPosition[col];
+            int currentConflicts;
 
-            for (int i = col + queens.length; i < conflictsOnPosition.length; i += queens.length) {
-                if (leastVal > conflictsOnPosition[i]) {
+            for (int row = 0; row < queens.length; row++) {
+                currentConflicts = conflicts(row, col, queens);
+                if (currentConflicts < minConflicts) {
                     small.clear();
-                    small.add(i);
-                    leastVal = conflictsOnPosition[i];
-                } else if (leastVal == conflictsOnPosition[i]) {
-                    small.add(i);
+                    small.add(row);
+                    minConflicts = currentConflicts;
+                } else if (currentConflicts == minConflicts) {
+                    small.add(row);
                 }
             }
-            return small.get(random.nextInt(small.size())) / queens.length;
+            return small.get(random.nextInt(small.size()));
         }
 
         public int queenBoardIndex(int col) {
@@ -215,11 +246,11 @@ public class NQueens {
 
         public boolean solved() {
             for (int col = 0; col < queens.length; col++) {
-                if (conflictsOnPosition[queenBoardIndex(col)] != 1) {
+                if (conflicts(queens[col], col, queens) != 0) {
                     return false;
                 }
             }
-            System.out.println(toString());
+//            System.out.println(toString());
             return true;
         }
 
@@ -255,3 +286,153 @@ public class NQueens {
     }
 
 }
+//import java.io.PrintStream;
+//import java.util.ArrayList;
+//import java.util.Random;
+//
+//public class NQueens {
+//
+//    private static class Board {
+//        Random random = new Random();
+//
+//        /**
+//         * The row for each column, For example [3,7,0,4,6,1,5,2] represents
+//         * <pre>
+//         *     ..Q.....
+//         *     .....Q..
+//         *     .......Q
+//         *     Q.......
+//         *     ...Q....
+//         *     ......Q.
+//         *     ....Q...
+//         *     .Q......
+//         * </pre>
+//         */
+//        int[] rows;
+//
+//        /**
+//         * Creates a new n x n board and randomly fills it with one
+//         * queen in each column.
+//         */
+//        Board(int n) {
+//            rows = new int[n];
+//            scramble();
+//        }
+//
+//        /**
+//         * Randomly fills the board with one queen in each column.
+//         */
+//        void scramble() {
+//            for (int i = 0, n = rows.length; i < n; i++) {
+//                rows[i] = i;
+//            }
+//            for (int i = 0, n = rows.length; i < n; i++) {
+//                int j = random.nextInt(n);
+//                int rowToSwap = rows[i];
+//                rows[i] = rows[j];
+//                rows[j] = rowToSwap;
+//            }
+//        }
+//
+//        /**
+//         * Returns the number of queens that conflict with (row,col), not
+//         * counting the queen in column col.
+//         */
+//        int conflicts(int row, int col) {
+//            int count = 0;
+//            for (int c = 0; c < rows.length; c++) {
+//                if (c == col) continue;
+//                int r = rows[c];
+//                if (r == row || Math.abs(r-row) == Math.abs(c-col)) count++;
+//            }
+//            return count;
+//        }
+//
+//        /**
+//         * Fills the board with a legal arrangement of queens.
+//         */
+//        void solve() {
+//            int moves = 0;
+//
+//            // This would be a lot faster if we used arrays of ints instead.
+//            ArrayList<Integer> candidates = new ArrayList<Integer>();
+//
+//            while (true) {
+//
+//                // Find nastiest queen
+//                int maxConflicts = 0;
+//                candidates.clear();
+//                for (int c = 0; c < rows.length; c++) {
+//                    int conflicts = conflicts(rows[c], c);
+//                    if (conflicts == maxConflicts) {
+//                        candidates.add(c);
+//                    } else if (conflicts > maxConflicts) {
+//                        maxConflicts = conflicts;
+//                        candidates.clear();
+//                        candidates.add(c);
+//                    }
+//                }
+//
+//                if (maxConflicts == 0) {
+//                    // Checked *every* queen and found no conflicts
+//                    System.out.println("MOVES:::" + moves);
+//                    System.exit(1);
+//                }
+//
+//                // Pick a random queen from those that had the most conflicts
+//                int worstQueenColumn =
+//                        candidates.get(random.nextInt(candidates.size()));
+//
+//                // Move her to the place with the least conflicts.
+//                int minConflicts = rows.length;
+//                candidates.clear();
+//                for (int r = 0; r < rows.length; r++) {
+//                    int conflicts = conflicts(r, worstQueenColumn);
+//                    if (conflicts == minConflicts) {
+//                        candidates.add(r);
+//                    } else if (conflicts < minConflicts) {
+//                        minConflicts = conflicts;
+//                        candidates.clear();
+//                        candidates.add(r);
+//                    }
+//                }
+//
+//                if (!candidates.isEmpty()) {
+//                    rows[worstQueenColumn] =
+//                            candidates.get(random.nextInt(candidates.size()));
+//                }
+//
+//                moves++;
+//                if (moves == rows.length * 2) {
+//                    // Trying too long... start over.
+//                    scramble();
+//                    moves = 0;
+//                }
+//            }
+//        }
+//
+//        /**
+//         * Prints the board, crudely, to a print stream.
+//         */
+//        void print(PrintStream stream) {
+//            for (int r = 0; r < rows.length; r++) {
+//                for (int c = 0; c < rows.length; c++) {
+//                    stream.print(rows[c] == r ? 'Q' : '.');
+//                }
+//                stream.println();
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Runs the application.
+//     */
+//    public static void main(String[] args) {
+//        Board board = new Board(2500);
+//        long start = System.currentTimeMillis();
+//        board.solve();
+//        long stop = System.currentTimeMillis();
+//        System.out.println("Found in " + ((double)(stop-start))/1000 + "s.");
+//        board.print(System.out);
+//    }
+//}
